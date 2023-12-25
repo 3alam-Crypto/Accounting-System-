87d38ae8846ac40562007c9321f06f4143f5e80d
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2023-12-25 13:44:17 +0300

    Update index.blade.php

--- resources/views/admin/sale/index.blade.php
+++ resources/views/admin/sale/index.blade.php
@@ -11,7 +11,8 @@
                         <div class="d-flex justify-content-between mb-3">
                             <h3 class="card-title">Sales</h3>
                             <!--begin::Primary button-->
-                            <a href="{{ route('create-sale') }}" class="btn btn-sm fw-bold btn-primary px-4 py-2">Add Sale</a>
+                            <a href="{{ route('create-sale') }}"
+                                class="btn btn-sm fw-bold btn-primary px-4 py-2">Add Sale</a>
                             <!--end::Primary button-->
                         </div>
 
@@ -35,21 +36,56 @@
                             </thead>
                             <tbody>
                                 @foreach($sales as $sale)
-                                <tr>
-                                    <td>{{ $sale->shipping_date }}</td>
-                                    <td>{{ $sale->our_order_id }}</td>
-                                    <td>{{ $sale->order_date }}</td>
-                                    <td>{{ $sale->customer_name }}</td>
-                                    <td>{{ $sale->product_name }}</td>
-                                    <td>{{ $sale->product_model }}</td>
-                                    <td style="display: flex; align-items: center;">
-                                        <a href="{{ route('edit-sale', $sale->id)}}" class="btn btn-success">Edit</a>
-                                        <a href="{{ route('view-sale', $sale->id)}}" class="btn btn-success space">View</a>
-                                        <a href="{{ route('createFile-sale', $sale->id) }}" class="btn btn-success space">Add File</a>
-                                        <a href="{{ route('viewFile-sale', $sale->id) }}" class="btn btn-success space">View File</a>
+                                    <tr>
+                                        <td>{{ $sale->shipping_date }}</td>
+                                        <td>{{ $sale->our_order_id }}</td>
+                                        <td>{{ $sale->order_date }}</td>
+                                        <td>{{ $sale->customer_name }}</td>
+                                        <td>{{ $sale->product_name }}</td>
+                                        <td>{{ $sale->product_model }}</td>
+                                        <td style="display: flex; align-items: center;">
 
-                                    </td>
-                                </tr>
+
+
+
+                                        </td>
+                                        <td class="text-end">
+                                            <a href="#"
+                                                class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm"
+                                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
+                                                <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
+                                            <!--begin::Menu-->
+                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
+                                                data-kt-menu="true">
+                                                <!--begin::Menu item-->
+                                                <div class="menu-item px-3">
+                                                    <a href="{{ route('edit-sale', $sale->id) }}"
+                                                        class="menu-link px-3">Edit</a>
+                                                </div>
+                                                <!--end::Menu item-->
+                                                <!--begin::Menu item-->
+                                                <div class="menu-item px-3">
+                                                    <a href="{{ route('view-sale', $sale->id) }}"
+                                                        class="menu-link px-3">View</a>
+                                                </div>
+                                                <!--end::Menu item-->
+                                                <!--begin::Menu item-->
+                                                <div class="menu-item px-3">
+                                                    <a href="{{ route('createFile-sale', $sale->id) }}"
+                                                        class="menu-link px-3">Add File</a>
+                                                </div>
+                                                <!--end::Menu item-->
+                                                <!--begin::Menu item-->
+                                                <div class="menu-item px-3">
+                                                    <a href="{{ route('viewFile-sale', $sale->id) }}"
+                                                        class="menu-link px-3">View Files</a>
+                                                </div>
+                                                <!--end::Menu item-->
+
+                                            </div>
+                                            <!--end::Menu-->
+                                        </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -63,14 +99,15 @@
 @endsection
 
 @section('javascript')
-    <script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
-    <script>
-        new DataTable('#kt_table_users',{
+<script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+<script>
+    new DataTable('#kt_table_users', {
 
 
-        }).on("draw", function () {
+    }).on("draw", function () {
         KTMenu.createInstances();
-        });
-    </script>
+    });
+
+</script>
 
 @endsection
